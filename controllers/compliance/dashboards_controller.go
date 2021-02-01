64ea4ccb3c23ae1a9909fac346cecea81519a00d
EvilAcid <achim.grolimund@hf-ict.info>
2021-02-01 20:27:22 +0100

    Add function "FidDashboard"

--- controllers/compliance/dashboards_controller.go
+++ controllers/compliance/dashboards_controller.go
@@ -4,6 +4,31 @@ import (
 	"jsontest/models/compliance"
 )
 
+// FindDashbord return true if the Dashboard exist by the user and id
+//
+//	dashboardID := "123XY"
+//	userID := "xyz123"
+//	if compliance.FindDashbord(&database, userID, dashboardID ){
+//		fmt.Printf("Dashbord with ID: %s exists on User with ID: %s", dashboardID, userID)
+//	}
+func FindDashbord(compliDB *compliance.ComplianceDB, userID string, dashboardID string) bool {
+	for i := range compliDB.Users {
+		user := &compliDB.Users[i]
+		if user.ID != userID {
+			continue
+		}
+		for j := range user.Dashboards {
+			dashboard := &user.Dashboards[j]
+			if dashboard.ID == dashboardID {
+				return true
+			}
+		}
+		return false
+	}
+	return false
+}
+
+// UpdateDashboards Update a existand Dasboard with an given new type of Dashboard
 func UpdateDashboards(compliDB *compliance.ComplianceDB, userID string, newDashboard *compliance.Dashboards) {
 	for i := range compliDB.Users {
 		user := &compliDB.Users[i]
